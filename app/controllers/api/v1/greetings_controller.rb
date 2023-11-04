a7f5e912902d21546c08e0e3e09d8c9d9c775b2a
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-03 19:53:18 -0700

    new push

--- app/controllers/api/v1/greetings_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::GreetingsController < ApplicationController
-  def index
-    @greetings = Greeting.all
-    render json: @greetings, status: :ok
-  end
-end
--- db/migrate/20231031174653_rename_message_to_greeting.rb
+++
@@ -1,5 +0,0 @@
-class RenameMessageToGreeting < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :greetings, :message, :greeting
-  end
-end
